83251a2c8ec8e75fe89051c7909e69283535dcbd
Faizal Adhitama Prabowo <faizaladhitamaprabowo@gmail.com>
2016-10-10 20:29:23 +0700

    cssnya belom

---
+++ TP 1 PPW/cobaRandom.js
@@ -0,0 +1,88 @@
+var randomItem = function(){
+	var lebar = 4;
+	var panjang = 4;
+	var total = lebar*panjang/2;
+	var item = [];
+
+	for(var i =0; i < total;i++){
+		item.push(i);
+		item.push(i);
+	}
+	//var item = [0,0,1,1,2,2,3,3,4,4,5,5,6,6,7,7];
+	var newArray = [];
+	while(item.length > 0){
+		var pushedItem = item[Math.floor(Math.random()*item.length)];
+		newArray.push(pushedItem);
+		var index = item.indexOf(pushedItem);
+		var deleted = item.splice(index, 1);
+	}
+	return newArray;
+}
+
+var loadImage = function(array){
+	var output = "";
+	while(array.length > 0){
+		var loadItem = array[Math.floor(Math.random()*array.length)];
+		var imageContainer = document.getElementById("flex-container");
+		if(imageContainer !=null){
+			var output = output + "<img id='emo" + loadItem +".jpg'"+ "src='cardSleeve.png'></div>";
+			document.getElementById("flex-container").innerHTML = output;
+		}
+		var index = array.indexOf(loadItem);
+		var deleted = array.splice(index, 1);
+	}
+}
+
+var loadSleeve = function(){
+	var output = "";
+	for(var i = 0; i < 16;i++){
+		var imageContainer = document.getElementById("flex-container");
+		if(imageContainer !=null){
+			var output = output + "<img src=\"cardSleeve.png\"></div>";
+			document.getElementById("flex-container").innerHTML = output;
+		}
+	}
+}
+
+var loginChecker = function(){
+	var usernameValue = document.getElementById("username").value;
+	var passwordValue = document.getElementById("password").value
+
+	var xmlhttp;
+	var output = "";
+	if(window.XMLHttpRequest){
+		xmlhttp = new XMLHttpRequest();
+	}
+	else{
+		xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+	}
+
+	xmlhttp.onreadystatechange = function(){
+		if(xmlhttp.readyState == 4 && xmlhttp.status == 200){
+			var userData = JSON.parse(xmlhttp.responseText);
+			var users = userData.users;
+			for(var i = 0; i < users.length;i++){
+				//console.log(users[i]);
+				var user = users[i];
+				var name = user.username;
+				var password = user.password;
+
+				if(usernameValue == name && passwordValue == password){
+					document.getElementById("user").innerHTML = name;
+				}
+			}
+		}
+	}
+
+	xmlhttp.open("GET","users.json",true);
+	xmlhttp.overrideMimeType("application/json");
+	xmlhttp.send();
+	$("#login").fadeOut("slow");
+	$("#game").fadeIn(3000);
+	return false;
+}
+
+var clearHistory = function(){
+	document.getElementById("rank").innerHTML = "";
+	localStorage.removeItem("historyRank");
+}
---
+++ TP 1 PPW/jquery.js
@@ -0,0 +1,143 @@
+$(document).ready(function(){
+	var counter = 0;
+	var arrayIndex = [];
+	var arrayGambar = [];
+	var solved = 0;
+	var done = false;
+	
+	$("#login").hide();
+	$("#welcome").delay("slow").show(1000);
+	$("#welcome").delay("slow").hide(2000);
+	$("#project").delay("slow").show(4000);
+	$("#transitionScreen").delay(6000).hide(9000);
+	$("#login").delay(6000).show(9000);
+	function UserRecord(username,time){
+		this.username = username;
+		this.time = time;
+
+		this.getUsername = function(){
+			return this.username;	
+		}
+
+		this.getTime = function(){
+			return this.time;
+		}
+	}
+
+	var sortArray = function(array){
+		if(typeof array == "Object"){
+			array.sort(function(a,b){
+				return parseInt(a)-parseInt(b);
+			});
+		}
+	}
+
+	var addRank = function(array){
+		console.log("add rank :" + array.length);
+		var output = "";
+		for(var i = 0; i < array.length;i++){
+			var obj = array[i];
+			var username = obj.username;
+			var time = obj.time;
+			output+="<li>"+username + " - " +savedToString(time)+"</li>";
+		}
+		document.getElementById("rank").innerHTML = output;
+	}
+
+	if(localStorage["historyRank"] != undefined){
+		historyArray = JSON.parse(localStorage["historyRank"]);
+		sortArray(historyArray);
+		addRank(historyArray);
+	}
+
+	$("body").on("click","#gameStart",function(){
+		solved = 0;
+		loadImage(randomItem());
+		stopTimer();
+		startTimer();
+
+		$('body').on('click','img',function(){
+		var source = $(this).attr("src");
+		var idImage = $(this).attr("id");
+		var indexGambar = $(this).index();
+
+			if(source == "cardSleeve.png"){
+				if(arrayIndex[0] != indexGambar && counter < 2){
+					$(this).prop('src',idImage);
+					arrayGambar.push(idImage);
+					arrayIndex.push(indexGambar);
+					counter++;
+					if(counter == 2){
+						checkImage(arrayIndex,arrayGambar);
+						arrayIndex = [];
+						arrayGambar = [];
+						counter = 0;
+						console.log(solved);
+						if(solved == 7){
+							pauseTimer();
+							var username = document.getElementById("username").value;
+							if(localStorage["historyRank"] == undefined){
+
+								var historyArray = [];
+								var userRec = new UserRecord(username,pauseTimer());
+								historyArray.push(userRec);
+								localStorage["historyRank"] = JSON.stringify(historyArray);
+								var history = JSON.parse(localStorage["historyRank"]);
+							}
+							else{
+								var historyArray = JSON.parse(localStorage["historyRank"]);
+								console.log(historyArray);
+								historyArray.push(new UserRecord(username,pauseTimer()));
+								localStorage["historyRank"] = JSON.stringify(historyArray);
+							}
+							sortArray(historyArray);
+							addRank(historyArray);
+							done = true;
+							return;
+						}
+					}
+				}
+			}
+		});
+		var checkImage = function(arrayIndex,arrayGambar){
+			setTimeout(function(){
+				if(arrayGambar[0] != arrayGambar[1]){
+					var gambar0 = document.getElementById("flex-container").childNodes[arrayIndex[0]];
+					var gambar1 = document.getElementById("flex-container").childNodes[arrayIndex[1]];
+					gambar0.src = "cardSleeve.png";
+					gambar1.src = "cardSleeve.png";
+				}
+				else{
+					solved+=1;
+				}
+			},500);
+		}
+	});
+
+
+	var randomItem = function(){
+		var item = [0,0,1,1,2,2,3,3,4,4,5,5,6,6,7,7];
+		var newArray = [];
+		while(item.length > 0){
+			var pushedItem = item[Math.floor(Math.random()*item.length)];
+			newArray.push(pushedItem);
+			var index = item.indexOf(pushedItem);
+			var deleted = item.splice(index, 1);
+		}
+		return newArray;
+	}
+
+	var loadImage = function(array){
+		var output = "";
+		while(array.length > 0){
+			var loadItem = array[Math.floor(Math.random()*array.length)];
+			var imageContainer = document.getElementById("flex-container");
+			if(imageContainer !=null){
+				var output = output + "<img id='emo" + loadItem +".jpg'"+ "src='cardSleeve.png'></div>";
+				document.getElementById("flex-container").innerHTML = output;
+			}
+			var index = array.indexOf(loadItem);
+			var deleted = array.splice(index, 1);
+		}
+	}
+});
