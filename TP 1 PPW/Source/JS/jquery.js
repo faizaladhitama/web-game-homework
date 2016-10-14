cd6d801d18186520ed2bc50b09c8dbb0ad603c1f
Faizal Adhitama Prabowo <faizaladhitamaprabowo@gmail.com>
2016-10-14 11:34:07 +0700

    fix

--- TP 1 PPW/Source/JS/jquery.js
+++ TP 1 PPW/Source/JS/jquery.js
@@ -2,54 +2,49 @@ $(document).ready(function(){
 	var counter = 0;
 	var arrayIndex = [];
 	var arrayGambar = [];
-	var solved = 0;
 	var done = false;
+	var solved = 0;
 	
-	// $("#login").hide();
-	// $("#welcome").delay("slow").fadeIn(3000);
-	// $("#welcome").delay("slow").fadeOut(1000);
-	// $("#project").delay(3000).fadeIn(3000);
-	// $("#project").delay("slow").fadeOut(2000);
-	// $("#transitionScreen").delay(9000).fadeOut(2000);
-	// $("#login").delay(9000).fadeIn(2000);
+	welcomeAnimation();
 
-	//load rank saat awal
 	if(localStorage["historyRank"] != undefined){
 		historyArray = JSON.parse(localStorage["historyRank"]);
 		sortArray(historyArray);
 		addRank(historyArray);
 	}
 
-	//ngeclick start
+	//click start
 	$("body").on("click","#gameStart",function(){
-		solved = 0;
+		var solved = 0;
 		loadImage(randomItem());
 		stopTimer();
 		startTimer();
 
-		//ngeclick gambar
+		//click gambar
 		$('body').on('click','img',function(){
 		var source = $(this).attr("src");
 		var idImage = $(this).attr("id");
 		var indexGambar = $(this).index();
-			//ngeclick penutup
-			if(source == "cardSleeve.png"){
+
+			//gambar yang diclick merupakan sleeve
+			if(source == "Source/Images/CardSleeve/cardSleeve3.png"){
 				if(arrayIndex[0] != indexGambar && counter < 2){
 					$(this).prop('src',idImage);
 					arrayGambar.push(idImage);
 					arrayIndex.push(indexGambar);
 					counter++;
-					//2 gambar sama
+					//2 gambar terbuka
 					if(counter == 2){
-						checkImage(arrayIndex,arrayGambar);
+						solved = checkImage(arrayIndex,arrayGambar,solved);
 						arrayIndex = [];
 						arrayGambar = [];
 						counter = 0;
-						//semua gambar solved
+						//permainan selesai
 						if(solved == 7){
 							pauseTimer();
 							var username = document.getElementById("username").value;
 							if(localStorage["historyRank"] == undefined){
+
 								var historyArray = [];
 								var userRec = new UserRecord(username,pauseTimer());
 								historyArray.push(userRec);
@@ -58,7 +53,6 @@ $(document).ready(function(){
 							}
 							else{
 								var historyArray = JSON.parse(localStorage["historyRank"]);
-								console.log(historyArray);
 								historyArray.push(new UserRecord(username,pauseTimer()));
 								localStorage["historyRank"] = JSON.stringify(historyArray);
 							}
---
+++ TP 1 PPW/Source/JS/stopwatch.js
@@ -0,0 +1,99 @@
+/*Sumber
+Copyright (c) 2010-2015 Giulia Alfonsi <electric.g@gmail.com>
+https://gist.github.com/electricg/4372563
+*/
+var timer = function() {
+    
+    var start = 0;
+    var savedTime = 0;
+    var timerOn = false;
+    var saveCounter = 0;
+
+    var now = function() {
+        var date = new Date();
+        return (date).getTime(); 
+    }; 
+
+    this.start = function() {
+        start = start ? start : now();
+        timerOn = true;
+        saveCounter=0;
+    };
+
+    this.stop = function() {
+       timerOn = false;
+       savedTime = start = 0;
+    };
+
+    this.save = function(){
+        saveCounter +=1;
+        if(timerOn && saveCounter==1){
+            savedTime = savedTime + now()-start;
+            start = 0;
+        }
+    }
+
+    this.time = function() {
+        return savedTime+(start?now()-start:0);
+    };
+
+    this.getSavedTime = function(){
+        return savedTime;
+    }
+};
+
+
+
+/*Sumber
+Copyright (c) 2010-2015 Giulia Alfonsi <electric.g@gmail.com>
+https://gist.github.com/electricg/4372563
+*/
+var timer = new timer();
+var $time;
+var stopwatch;
+
+var digitTime = function(angka, digit) {
+    var stringWaktu = "00" + angka;
+    return stringWaktu.substr(stringWaktu.length-digit);
+}
+
+var formatTime = function (time) {
+    var h = m = s = ms = totalSec = 0;
+    var newTime = "";
+    totalSec = time/1000;
+    ms = time%1000;
+    s = Math.floor(totalSec%60);
+    m = Math.floor((totalSec%3600)/60);
+    newTime = digitTime(m, 2) + ':' + digitTime(s, 2) + ':' + digitTime(ms, 2);
+    return newTime;
+}
+
+var showTime = function() {
+    $time = document.getElementById("time");
+    timeUpdate();
+}
+
+var timeUpdate = function() {
+    $time.innerHTML = formatTime(timer.time());
+}
+
+var startTimer = function() {
+    stopwatch = setInterval("timeUpdate()", 0.1);
+    timer.start();
+}
+
+var stopTimer =function() {
+    clearInterval(stopwatch);
+    timer.stop();
+    timeUpdate();
+}
+
+var pauseTimer = function(){
+    timer.save();
+    clearInterval(stopwatch);
+    return timer.getSavedTime();
+}
+
+var savedToString = function(time){
+    return formatTime(time);
+}
